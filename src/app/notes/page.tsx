a9ed9622c6ed1da5772676f1d752b5692f858fee
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-04 00:58:09 +0100

    add styling for notes cards

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,3 +1,4 @@
+import Note from "@/components/Note"
 import prisma from "@/lib/db/prisma"
 import { auth } from "@clerk/nextjs"
 import { Metadata } from "next"
@@ -14,5 +15,14 @@ export default async function NotesPage(){
     const allNotes = await prisma.notes.findMany({where: {userId}})
 
 
-    return <div>{JSON.stringify(allNotes)}</div>
+    return (
+    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3"> {allNotes.map((note)=>{
+        return(
+        <Note note={note} key={note.id}/>)
+    })} 
+    {allNotes.length===0 && (
+        <div className="col-span-full text-center">{"Your notes are empty. Click on Add Note button to create a note"}</div>
+    )} </div>
+    )
+
 }
