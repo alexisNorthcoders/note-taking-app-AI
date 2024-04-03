fb17643faa97174f370bd9ecd9cf0883782162fe
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-04 00:16:35 +0100

    add component to add notes, turn navbar from server to client rendering

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -1,12 +1,18 @@
+"use client"
+
 import Link from "next/link";
 import logo from "@/assets/logo.png";
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
+import { useState } from "react";
+import AddNoteDialog from "@/components/AddNoteDialog";
 
 export default function NavBar() {
+    const [showAddNoteDialog,setShowAddNoteDialog] = useState(false)
   return (
+    <>
     <div className="p-4 shadow">
       <div className="item-center m-auto flex max-w-7xl flex-wrap justify-between gap-3">
         <Link href="/notes" className="flex items-center gap-1">
@@ -15,12 +21,14 @@ export default function NavBar() {
         </Link>
         <div className="flex items-center gap-2">
             <UserButton afterSignOutUrl="/" appearance={{elements:{avatarBox:{width:"2.5rem",height:"2.5rem"}}}}/>
-            <Button>
+            <Button onClick={() => setShowAddNoteDialog(true)}>
                 <Plus size={20} className="mr-2"/>
                 Add Note
             </Button>
         </div>
       </div>
     </div>
+    <AddNoteDialog open={showAddNoteDialog} setOpen={setShowAddNoteDialog}/>
+    </>
   );
 }
