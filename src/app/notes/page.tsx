66e2f9434fbf371fae7668dc7af838e1a2f69321
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-03 19:02:56 +0100

    add authentication with clerk, add different routes for signin/signup pages, add titles

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Chat AI - Your Notes"
+}
+
+export default function NotesPage(){
+    return <div>Notes here</div>
+}
---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from "@clerk/nextjs";
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Chat AI - SignIn"
+}
+
+export default function SignInPage(){
+    return <div className="flex h-screen items-center justify-center">
+        <SignIn appearance={{variables: {colorPrimary:"#2C2A4A"}}}/>
+    </div>
+}
