3db68f68d4f0bf01b479b6a33f928170ef06cb36
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-03 23:53:39 +0100

    add navbar, frontpage, redirection when logged in

---
+++ src/app/notes/NavBar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
+export default function NavBar() {
+  return (
+    <div className="p-4 shadow">
+      <div className="item-center m-auto flex max-w-7xl flex-wrap justify-between gap-3">
+        <Link href="/notes" className="flex items-center gap-1">
+          <Image src={logo} alt="chat logo" width={40} height={40} />
+          <span className="font-bold">Chat AI</span>
+        </Link>
+        <div className="flex items-center gap-2">
+            <UserButton afterSignOutUrl="/" appearance={{elements:{avatarBox:{width:"2.5rem",height:"2.5rem"}}}}/>
+            <Button>
+                <Plus size={20} className="mr-2"/>
+                Add Note
+            </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
