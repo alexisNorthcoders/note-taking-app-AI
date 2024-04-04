fed9528b52e8b526bfc1f0ae0cceea25aa737d56
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-04 17:24:49 +0100

    revert changes to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)", 
-    "/", 
-    "/(api|trpc)(.*)", 
-    "!/favicon.ico" 
-  ],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
