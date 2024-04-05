28a24f74781b11ff5a1363d9f7b46d8078e25d69
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-05 16:41:38 +0100

    refactor notes/page to SSR

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,62 +1,31 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import Note from "@/components/Note";
-import { setCookie, parseCookies } from "nookies";
-import { useRouter } from 'next/navigation';
+import Note from "@/components/Note"
+import prisma from "@/lib/db/prisma"
+import { Metadata } from "next"
+import { cookies } from "next/headers";
 
-export default function NotesPage() {
-  const [allNotes, setAllNotes] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const router = useRouter();
+export const metadata: Metadata = {
+    title: "Notes AI"
+}
+
+export default async function NotesPage(){
+  const parsedCookie = cookies().get("userId");
+  const cookieValue = parsedCookie?.value || null;
+  const parsedCookieValue = cookieValue ? JSON.parse(cookieValue) : {};
+  const userId = parsedCookieValue.userId;
+
+    if (!userId) throw Error("userId undefined")
+
+    const allNotes = await prisma.notes.findMany({where: {userId}})
 
 
-  useEffect(() => {
-    const cookies = parseCookies();
-    const parsedCookie = JSON.parse(cookies.userId);
-    const userId = parsedCookie.userId
+    return (
+    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3"> {allNotes.map((note)=>{
+        return(
+        <Note note={note} key={note.id}/>)
+    })} 
+    {allNotes.length===0 && (
+        <div className="col-span-full text-center">{"Your notes are empty. Click on Add Note button to create a note"}</div>
+    )} </div>
+    )
 
-    if (!userId) {
-      setIsLoading(false);
-      router.push('/sign-in'); 
-      return;
-    }
-
-    const fetchNotes = async () => {
-      try {
-        const response = await fetch(`/api/prisma/${userId}`);
-
-        if (!response.ok) {
-          throw new Error("Network error");
-        }
-
-        const { allNotes } = await response.json();
-
-        setAllNotes(allNotes);
-       
-      } catch (error) {
-        throw Error();
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchNotes();
-  }, [router]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
-      {allNotes.map((note, index) => (
-        <Note note={note} key={index} />
-      ))}
-      {allNotes.length === 0 && (
-        <div className="col-span-full text-center">
-          {"Your notes are empty. Click on Add Note button to create a note"}
-        </div>
-      )}
-    </div>
-  );
 }
